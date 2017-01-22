1a32a5152ea3a3b7f23bc7d463954a62925795ba
Yang Bai <ybai62868@gmail.com>
2017-01-23 02:34:39 +0800

    Create #1043 : 完全背包.cpp

---
+++ #1043 : 完全背包.cpp
@@ -0,0 +1,20 @@
+# include <cstdio>
+# include <iostream>
+using namespace std;
+# define MAX 123456
+int need[MAX],value[MAX];
+int dp[MAX];
+int main(void)
+{
+	int n,m; scanf("%d%d",&n,&m);
+	for ( int i = 0;i < n;i++ ) {
+		scanf("%d%d",&need[i],&value[i]);
+	}
+	for ( int i = 0;i < n;i++ ) {
+		for ( int j = need[i];j <= m;j++ ) {
+			dp[j] = max(dp[j],dp[j-need[i]]+value[i]);
+		}
+	}
+	printf("%d\n",dp[m]);
+	return 0;
+}
